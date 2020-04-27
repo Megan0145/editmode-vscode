539ba7be5e8465c8a92017f59bfd0ea946c2f8d4
Megan Ennis <mennis0145@gmail.com>
2020-04-27 14:50:52 +0100

    Add error handling for inccorrect auth token or non-existent config file

--- editmode/extension.js
+++ editmode/extension.js
@@ -18,7 +18,7 @@ function activate(context) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "editmode.helloWorld",
+    "editmode.createChunk",
     function () {
       // The code you place here will be executed every time your command is executed
 
@@ -34,40 +34,51 @@ function activate(context) {
       vscode.workspace
         .openTextDocument(`${vscode.workspace.rootPath}/em.config.json`)
         .then((doc) => {
-          let settings = JSON.parse(doc.getText()); 
+          let settings = JSON.parse(doc.getText());
           let data = {
             bit: {
               content: text,
             },
             authentication_token: settings.em_authentication_token,
-		  };
-		
-		axios({
-			method: 'post',
-			url: 'https://www.editmode.app/api/v1/bits',
-			data: data,
-			headers: {
-				'Content-Type': 'application/json',
-				'User-Agent': 'Mozilla/5.0' 
-			}
-		  })
-		  .then(res => {
-			let snippet = settings.em_snippet_templates[settings.em_default_snippet_template];
-			let bit_identifier = res.data.identifier;
-			let bit_content = res.data.bit.content;
-			let bit_label = bit_content.substr(0,10) + "..";
-			snippet = snippet.replace("{identifier}", bit_identifier);
-			snippet = snippet.replace("{label}", bit_label);
-			console.log("New snippet: " + snippet)
-			editor.edit((editBuilder) => {
-				editBuilder.replace(selection, snippet);
-			  });
-		  })
-		  .catch(err => console.log("Fail" + err.message))
+          };
+
+          axios({
+            method: "post",
+            url: "https://www.editmode.app/api/v1/bits",
+            data: data,
+            headers: {
+              "Content-Type": "application/json",
+              "User-Agent": "Mozilla/5.0",
+            },
+          })
+            .then((res) => {
+              let snippet =
+                settings.em_snippet_templates[
+                  settings.em_default_snippet_template
+                ];
+              let bit_identifier = res.data.identifier;
+              let bit_content = res.data.bit.content;
+              let bit_label = bit_content.substr(0, 10) + "..";
+              snippet = snippet.replace("{identifier}", bit_identifier);
+              snippet = snippet.replace("{label}", bit_label);
+              console.log("New snippet: " + snippet);
+              editor.edit((editBuilder) => {
+                editBuilder.replace(selection, snippet);
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              vscode.window.showErrorMessage(
+				`EditMode: We're sorry. Something went wrong trying to create a chunk. 
+				Please make sure all details in your EditMode configuration file are accurate and try again.`
+              );
+            });
         })
-        .catch((err) => console.log(err));
-
-
+        .catch(() => {
+          vscode.window.showWarningMessage(
+            `EditMode: Cannot find "em.config.json" file in root directory.`
+          );
+        });
     }
   );
 
